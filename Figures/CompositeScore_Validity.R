ce8e3c5fe5cfb985b8bb95f3f9e7599ca3fc2bbd
Stef Herregods <herregodsstef@hotmail.com>
2021-11-28 17:13:43 +0100

    Outline validity plot

--- Figures/CompositeScore_Validity.R
+++ Figures/CompositeScore_Validity.R
@@ -1,3 +1,25 @@
 # November 2021
-# Generates graph based on simulated data
-# Separate item validity vs. composite validity
+# Generates heat map based on simulated data
+# Composite score validity vs. 'percentage additional information'
+
+## To do:
+# Is main level of low validity item important?
+# Is main level of high validity items important?
+# Heat map should be with two separate colors, not continuous
+# Also add continous figure? Because maybe when bad, it's very bad (and when good, not such a big bonus)
+
+
+# 1. Simulating data
+
+# Low validity item
+
+
+# High validity items
+
+
+# 2. Calculate composite scores (with and without low validity item)
+
+# 3. Calculate composite score validity for both composite scores
+
+# 4. Create figure
+
