bec4d577191d55174e3d881455c0c46cf3e12daa
Stef Herregods <herregodsstef@hotmail.com>
2022-03-09 12:39:10 +0100

    Create test_statistics.R

---
+++ Inference/test_statistics.R
@@ -0,0 +1,19 @@
+# 
+
+rm(list = ls(all = TRUE))
+
+
+### MEDIAN ESTIMATED EFFECT ###
+# (Simonsohn et al., 2020) 
+
+
+### SHARE OF STATISTICALLY SIGNIFICANT SPECIFICATIONS (dichotomous) #
+# (Simonsohn et al., 2020) 
+
+
+### SHARE OF STATISTICALLY SIGNIFICANT SPECIFICATIONS (continuous) #
+# (Simonsohn et al., 2020) 
+
+
+### NON-EXPERIMENTAL DATA ###
+# (Simonsohn et al., 2020)
